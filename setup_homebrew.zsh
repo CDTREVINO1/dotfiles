d9bfc9fdb168e70d8fab3b708a97acbb6f420b7f
CDTREVINO1 <29134300+CDTREVINO1@users.noreply.github.com>
2026-01-19 15:43:12 -0600

    Switch to Brewfile for Homebrew installs
    Install formula, casks and vscode extensions

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,30 +4,4 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Install Brew Formulae
-brew install python
-brew install yt-dlp
-brew install ffmpeg
-brew install mongodb-community
-brew install mongodb-database-tools
-brew install mongosh
-brew install node
-
-# Install MacOS Applications
-brew install --cask google-chrome
-brew install --cask firefox
-brew install --cask google-drive
-brew install --cask vlc
-brew install --cask ultimaker-cura
-brew install --cask visual-studio-code
-brew install --cask handbrake
-brew install --cask teamviewer
-brew install --cask rectangle
-brew install --cask kindle
-brew install --cask adobe-creative-cloud
-brew install --cask postman
-
-# Install Fonts
-brew install font-fira-code
-brew install font-source-code-pro
-brew install font-jetbrains-mono
+brew bundle --verbose
